990fb06582fb4f3d3d246ee84e43f8deb5bd5059
Burt Beckwith <burt@burtbeckwith.com>
2013-02-18 02:35:01 -0500

    cleanup

--- SanitizerGrailsPlugin.groovy
+++ SanitizerGrailsPlugin.groovy
@@ -1,29 +1,21 @@
-import org.codehaus.groovy.grails.validation.ApplicationContextAwareConstraintFactory;
-import org.codehaus.groovy.grails.validation.ConstrainedProperty;
-import org.grails.plugins.sanitizer.*;
+import org.codehaus.groovy.grails.validation.ApplicationContextAwareConstraintFactory
+import org.codehaus.groovy.grails.validation.ConstrainedProperty
+import org.grails.plugins.sanitizer.MarkupConstraint
 
 class SanitizerGrailsPlugin {
-    // the plugin version
-    def version = "0.8.0"
-    // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.3.1 > *"
-    // the other plugins this plugin depends on
-    def dependsOn = [:]
-    // resources that are excluded from plugin packaging
-    def pluginExcludes = [
-            "grails-app/views/error.gsp"
-    ]
+	def version = "0.8.0"
+	def grailsVersion = "1.3.1 > *"
 
-    def author = "Daniel Bower"
-    def authorEmail = "daniel@bowerstudios.com"
-    def title = "Grails Markup Sanitizer Plugin"
-    def description = '''\\
-Plugin for Sanitizing Markup(HTML, XHTML, CSS) using OWASP AntiSamy.  
-Filters malicious content from User generated content (such as that entered through Rich Text boxes).  
+	def author = "Daniel Bower"
+	def authorEmail = "daniel@bowerstudios.com"
+	def title = "Grails Markup Sanitizer Plugin"
+	def description = '''\
+Plugin for Sanitizing Markup(HTML, XHTML, CSS) using OWASP AntiSamy.
+Filters malicious content from User generated content (such as that entered through Rich Text boxes).
 
 Features -
 * Ruleset in web-app/WEB-INF/antisamy-policy.xml
-* Constraint "markup"     
+* Constraint "markup"
   - can be added to domain/command classes to validate that a string is valid and safe markup
   - important note:  The constraint is for validation only, it does not sanitize the string
 * Encoding-only Codec "myText.encodeAsSanitizedMarkup()"
@@ -31,7 +23,7 @@ Features -
   - (the codec uses the service, too)
 * MarkupSanitizerService
   - use the codec or the service to sanitize the string
-  - access in your controllers/services via 
+  - access in your controllers/services via
     	def markupSanitizerService
   - method MarkupSanitizerResult sanitize(String dirtyString)
   - method MarkupValidatorResult validateMarkup(String htmlString)
@@ -39,40 +31,17 @@ Features -
 
 Please note the beta nature of the version number.  This plugin has not been extensively tested.  Please feel
 free to send me any results of any testing you may do.
-    	
+
 This module does not sanitize a string that does not contain valid markup.  If it does not contain
 valid markup, it will simply return an empty string.
 '''
 
-    // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/sanitizer"
+	def documentation = "http://grails.org/plugin/sanitizer"
 
-    def doWithWebDescriptor = { xml ->
-    }
-
-    def doWithSpring = {
-    }
-
-    def doWithDynamicMethods = { ctx ->
-    }
-
-    def doWithApplicationContext = { applicationContext ->
-    	// Implement post initialization spring config (optional)    
-    	def factory = new ApplicationContextAwareConstraintFactory(
-	    		applicationContext, MarkupConstraint.class, ["markupSanitizerService"])
-	    ConstrainedProperty.registerNewConstraint(MarkupConstraint.MARKUP_CONSTRAINT, factory);
-    }
-
-    def onChange = { event ->
-        // Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-        
-        // TODO need to reload the antisamyservice only...
-    }
-
-    def onConfigChange = { event ->
-        // Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
+	def doWithApplicationContext = { applicationContext ->
+		// Implement post initialization spring config (optional)
+		def factory = new ApplicationContextAwareConstraintFactory(
+			applicationContext, MarkupConstraint, ["markupSanitizerService"])
+		ConstrainedProperty.registerNewConstraint(MarkupConstraint.MARKUP_CONSTRAINT, factory)
+	}
 }
--- test/integration/org/grails/plugins/sanitizer/SanitizedMarkupCodecIntegrationTests.groovy
+++ test/integration/org/grails/plugins/sanitizer/SanitizedMarkupCodecIntegrationTests.groovy
@@ -1,36 +1,27 @@
 package org.grails.plugins.sanitizer
 
-import org.codehaus.groovy.grails.commons.ConfigurationHolder;
-
 import grails.test.*
 
-class SanitizedMarkupCodecIntegrationTests extends GrailsUnitTestCase {
-    
-	protected void setUp() {
-        super.setUp()
-    }
+import org.codehaus.groovy.grails.commons.ConfigurationHolder
 
-    protected void tearDown() {
-        super.tearDown()
-    }
+class SanitizedMarkupCodecIntegrationTests extends GroovyTestCase {
 
-    void testHappyPath() {
-    	String my = "<p>my paragraph</p>"
-    
-        assertEquals("<p>my paragraph</p>", my.encodeAsSanitizedMarkup())
+	void testHappyPath() {
+		String my = "<p>my paragraph</p>"
 
-    }
-	
+		assertEquals("<p>my paragraph</p>", my.encodeAsSanitizedMarkup())
+	}
+
 	void testTrustedSanitizerFixTag() {
 		ConfigurationHolder.config?.sanitizer= [trustSanitizer: true]
-		
+
 		String my = "<p>my paragraph<p>"
 		assertEquals("<p>my paragraph</p>", my.encodeAsSanitizedMarkup())
 	}
-	
+
 	void testTrustedSanitizerStripScript() {
 		ConfigurationHolder.config?.sanitizer= [trustSanitizer: true]
-		
+
 		String my = "<script></script><p>my paragraph</p>"
 		assertEquals("<p>my paragraph</p>", my.encodeAsSanitizedMarkup())
 	}
