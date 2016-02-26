ba498842a45757a95455ab556d14497e29fbb68e
Daniel Bower <daniel.d.bower@gmail.com>
2016-02-25 21:17:23 -0500

    using version 2.5 of grails, version plugin to 0.15.0

--- SanitizerGrailsPlugin.groovy
+++ SanitizerGrailsPlugin.groovy
@@ -5,8 +5,8 @@ import org.grails.plugins.sanitizer.MarkupSanitizerService
 import org.springframework.core.io.ClassPathResource
 
 class SanitizerGrailsPlugin {
-	def version = "0.10.0"
-	def grailsVersion = "2.2.0 > *"
+	def version = "0.15.0"
+	def grailsVersion = "2.5.0 > *"
 
 	def author = "Daniel Bower"
 	def authorEmail = "daniel@bowerstudios.com"
